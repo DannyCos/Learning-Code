61f845e7f9fdb2a11934ad52fd65bb24c06968da
DannyCos <DannyCos@users.noreply.github.com>
2021-03-08 16:37:07 +0000

    Interactive prompt finished

--- C/interative_prompt/main.c
+++ C/interative_prompt/main.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <editline/readline.h>
-#include <editline/histedit.h>
+//Compiling on Windows machine
+#ifdef _WIN32
+#include <string.h>
 
-/* Declare a buffer for user input of size 2048 */
-static char input[2048];
+static char buffer[2048];
+
+// Fake readline function
+char* readline(char* prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer)+1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy)-1] = '\0';
+  return cpy;
+}
+
+// Fake add_history function
+void add_history(char* unused) {}
+
+#else
+#include <readline/readline.h>
+#include <readline/history.h>
+#endif
 
 int main(int argc, char** argv) {
 
@@ -17,14 +35,13 @@ int main(int argc, char** argv) {
   while (1) {
 
     /* Output our prompt */
-    char* input = readLine("lispy>");
+    char* input = readline("lispy>");
 
     /* Add input to history */
     add_history(input);
 
     /* Echo input back to user */
     printf("No you're a %s\n", input);
-
   }
 
   return 0;
